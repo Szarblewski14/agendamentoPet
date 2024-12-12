994955fc6c2703f2045dd44c7d360304de903f76
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2024-12-12 16:27:34 -0300

    testando importacao pelo index css

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@
 //importação da config do dayjs
 import "./libs/dayjs.js"
 
-//css
-import "./styles/form.css"
-import "./styles/global.css"
-import "./styles/schedule.css"
-
 
 //JS
 import "./modules/form/submit.js"
