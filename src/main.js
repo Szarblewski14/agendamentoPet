5add7899a2af04b8edd3a9cf5461c778796c11b3
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2024-12-13 12:20:48 -0300

    resolvendo problema de compilação

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 //importação da config do dayjs
 import "./libs/dayjs.js"
 
+//css
+import "./styles/form.css"
+import "./styles/global.css"
+import "./styles/schedule.css"
+
 
 //JS
 import "./modules/form/submit.js"
